49fffd3463daf0203bc1e06ccd969234ac0c6e60
youtanimstar <animdeep2019@gmail.com>
2025-02-08 21:38:15 +0530

    first commit

--- Class_Suru_Frontend/Frontend_Main/src/main.jsx
+++ Class_Suru_Frontend/Frontend_Main/src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Router.jsx";
-import { Provider } from 'react-redux'
-import store from './redux/store/store.js'
+// import { Provider } from 'react-redux'
+// import store from './redux/store/store.js'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <RouterProvider router={router} />
-    </Provider>
+    {/* </Provider> */}
   </StrictMode>
 );
